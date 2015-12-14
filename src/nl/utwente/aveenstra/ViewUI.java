ddf9825d83b282003bad57652b853938a64a9f79
Johanwv <johan_joto@hotmail.com>
2015-12-14 11:44:32 +0100

    videoButtonPattern

--- src/nl/utwente/aveenstra/ViewUI.java
+++ src/nl/utwente/aveenstra/ViewUI.java
@@ -42,7 +42,7 @@ public class ViewUI extends Application implements View {
     public static final String readyToRecordString = "Ready to record";
     public static final String recordingString = "Recording";
     public static final Pattern rNumberPattern = Pattern.compile("^[rR]?(1[1-3])|(2000)[0-9]+$");
-    public static final Pattern videoButtonPattern = Pattern.compile("^Webcam_R(?<rnumber>\\d{6})_(?<datum>\\d{2}-\\d{2}-\\d{2})\\.MPEG$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern videoButtonPattern = Pattern.compile("^Webcam_R(?<rnumber>\\d{6})_(?<datum>\\d{2}-\\d{2}-\\d{4})\\.MPEG$", Pattern.CASE_INSENSITIVE);
 
     public ArrayList<UpdatingScatterChart> charts = new ArrayList<>();
     private TextField author;
